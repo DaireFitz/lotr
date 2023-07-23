e9c830625f1b6cb4fa26adaa9f623fe2d5bd10e3
DaireFitz <131209841+DaireFitz@users.noreply.github.com>
2023-07-23 22:30:12 +0100

    Changed id to sindarin for fade in event

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
- function selectDifficulty(difficulty) {
-      //  code to handle the selected difficulty
-      window.location.href = 'quiz.html?difficulty=' + difficulty;
-    }
+// Function to handle the selected difficulty
+function selectDifficulty(difficulty) {
+  window.location.href = 'quiz.html?difficulty=' + difficulty;
+}
 
-    const welcomeText = document.getElementById('welcome-text');
-    const difficultyButtons = document.querySelector('.difficulty-buttons');
-    // Event to allow dificulty  options to fade in once user hovers over welcome text
-    welcomeText.addEventListener('mouseenter', () => {
-      difficultyButtons.style.opacity = 1;
-      difficultyButtons.style.pointerEvents = 'auto';
-    });
+const welcomeText = document.getElementById('welcome-text');
+const difficultyButtons = document.querySelector('.difficulty-buttons');
+// Event to allow difficulty options to fade in once the user hovers over <h1 class="Sindarin"
+welcomeText.addEventListener('mouseenter', () => {
+  difficultyButtons.style.opacity = 1;
+  difficultyButtons.style.pointerEvents = 'auto';
+});
 
-    welcomeText.addEventListener('mouseleave', () => {
-      difficultyButtons.style.opacity = 0;
-      difficultyButtons.style.pointerEvents = 'none';
-    });
+welcomeText.addEventListener('mouseleave', () => {
+  difficultyButtons.style.opacity = 0;
+  difficultyButtons.style.pointerEvents = 'none';
+});
