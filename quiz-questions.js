cce659a0162b3de936bf274f5d94028b905344bd
DaireFitz <131209841+DaireFitz@users.noreply.github.com>
2023-07-27 11:57:24 +0100

    Made JSON file and compiled Java Script

--- quiz-questions.js
+++ quiz-questions.js
@@ -345,3 +345,35 @@ const easyQuizData = [
       }
       return randomQuizData;
     }
+
+
+
+    
+function selectDifficulty(difficulty) {
+  window.location.href = 'quiz.html?difficulty=' + difficulty;
+}
+
+// Function to show difficulty buttons on mouseover
+function showDifficultyButtons() {
+  const difficultyButtons = document.getElementById('difficulty-buttons');
+  difficultyButtons.classList.add('fade-in'); // Added class to trigger fade in
+}
+
+
+function hideDifficultyButtons() {
+  const difficultyButtons = document.getElementById('difficulty-buttons');
+  difficultyButtons.style.opacity = 0;
+  difficultyButtons.style.pointerEvents = 'none';
+}
+
+const bannerText = document.getElementById('banner-text');
+bannerText.addEventListener('mouseenter', () => {
+  console.log('Mouse entered banner-text');
+  showDifficultyButtons();
+});
+
+
+function selectDifficulty(difficulty) {
+  // Redirect to the corresponding quiz page based on the selected difficulty
+  window.location.href = `quiz-${difficulty}.html`;
+}
